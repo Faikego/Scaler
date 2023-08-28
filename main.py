08c39143ab1a773d11efbc8557324a8d923cf636
faikego <f4llno@yandex.ru>
2023-08-28 16:58:00 +0400

    HZ

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import openpyxl
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+
 def date_comparison(x_date,y_date):
     def dot_seeker(dot,ifer):
         if ifer == 1:
@@ -78,29 +82,43 @@ def write_file(create_number,create_date,status,full_price,product_name,send_to_
             work_table.save('scaler_table.xlsx')
 def main():
     i=0
-    end_date = '27.07.2023'
-    service = Service(executable_path='\chromedriver.exe')
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(options=options)
-    driver.get('https://business.kazanexpress.ru/seller/4449/invoices/send')
-    time.sleep(15)
+    url=comber.get()
+    if url=="TOPS":
+        url='https://business.kazanexpress.ru/seller/4449/invoices/send'
+    elif url=="Стельки":
+        url='https://business.kazanexpress.ru/seller/65366/invoices/send'
+    elif url=='Триколор':
+        url='https://business.kazanexpress.ru/seller/10020/invoices/send'
+    elif url=='Джибитсы':
+        url='https://business.kazanexpress.ru/seller/51310/invoices/send'
+    elif url=='Discont OFF':
+        url='https://business.kazanexpress.ru/seller/10238/invoices/send'
+    end_date = entry_date.get()
+    try:
+        service = Service(executable_path='chromedriver.exe')
+        options = webdriver.ChromeOptions()
+        driver = webdriver.Chrome(options=options)
+    except:
+        driver = webdriver.ChromiumEdge()
+    driver.implicitly_wait(90)
+    driver.get(url)
+    #time.sleep(15)
     field_finder=driver.find_element(By.XPATH, "//*[@id='username']")
     field_finder.send_keys('f4llno@yandex.ru')
     field_finder=driver.find_element(By.XPATH, "//*[@id='password']")
-    field_finder.send_keys('Pravoe_del0')
+    field_finder.send_keys('Scaler_Password')
     time.sleep(1)
     driver.find_element(By.XPATH, "//*[@id='signin']/section/div/section[2]/form/button").click()
-    time.sleep(4)
-    driver.get('https://business.kazanexpress.ru/seller/4449/invoices/send')
-    time.sleep(1.6)
-
+    time.sleep(2)
+    driver.get(url)
+    #time.sleep(1.6)
     while True:
         element = driver.find_element(By.XPATH, "//*[@id='status-cell-" + str(i) + "-73-0']/div")
         driver.execute_script("arguments[0].scrollIntoView(true);", element)
         driver.find_element(By.XPATH, "//*[@id='status-cell-" + str(i) + "-73-0']/div").click()
-        time.sleep(0.8)
+        #time.sleep(0.8)
         create_number=(driver.find_element(By.XPATH, "//*[@id='openInvoice']/div/div/div[2]/span[2]").text)
-        print(create_number)
+        #print(create_number)
         create_date=driver.find_element(By.XPATH,'//*[@id="openInvoice"]/div/div/div[2]/span[4]').text
         status=driver.find_element(By.XPATH,'//*[@id="openInvoice"]/div/div/div[2]/span[6]').text
         if status=='Принята на складе':
@@ -119,5 +137,24 @@ def main():
             write_file(create_number,create_date,status,full_price,product_name,send_to_house,get_on_house,purchase_price)
             i=i+1
             driver.find_element(By.XPATH,'//*[@id="openInvoice"]/div/header/div[2]').click()
-if __name__=='__main__':
-    main()
+# def scaler_window():
+magazines = ['TOPS', 'Стельки', 'Триколор', 'Джибитсы', 'Discont OFF']
+window = tk.Tk()
+window.title('Scaler')
+window ['bg'] = 'gray10'
+date_label = tk.Label(text="Введите дату (ДД.ММ.ГГГГ)",background='gray10',foreground='white')
+#date_label ['bg'] = 'gray10'
+date_label.pack()
+entry_date = tk.Entry()
+entry_date.pack()
+magazine_label = tk.Label(text="Выберите магазин",background='gray10',foreground='white')
+#magazine_label ['bg'] = 'gray10'
+magazine_label.pack()
+comber = ttk.Combobox(values=magazines)
+comber.pack()
+button = tk.Button(text='Начать выполнение', command=main)
+button ['bg'] = 'white'
+button.pack()
+window.mainloop()
+# if __name__=='__main__':
+#     scaler_window()
